4c083ee35055bb7331d449475263a537fb5f251a
nik <nik_rb@yahoo.com>
2016-08-13 20:49:35 +0100

    scratch out purchase form layout

--- src/js/pages/Purchase.js
+++ src/js/pages/Purchase.js
@@ -3,8 +3,41 @@ import React from "react";
 export default class Purchase extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container" >
         <h1>Purchase</h1>
+        <form className="well">
+          <div className="form-group" >
+            <label htmlFor="category" className="col-sm-2 control-label">Category</label>
+              <select id="category" className="input-sm">
+                <option>Food</option>
+              </select>
+          </div>
+          <div className="form-group" >
+            <label htmlFor="product_name" className="col-sm-2 control-label">Name</label>
+            <input id="product-name" className="input-sm" />
+          </div>
+          <div className="form-group" >
+            <label htmlFor="amount" className="col-sm-2 control-label">Amount</label>
+            <input id="amount" className="input-sm" />
+          </div>
+          <div className="form-group" >
+            <label htmlFor="cost_per" className="col-sm-2 control-label" >Cost Per</label>
+            <input id="cost_per" className="input-sm"/>
+            <select className="input-sm">
+              <option>item</option>
+              <option>kilo</option>
+              <option>gram</option>
+            </select>
+          </div>
+          <div className="form-group" >
+            <label htmlFor="cost" className="col-sm-2 control-label" >Cost</label>
+            <input className="input-sm" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="create_button" className="col-sm-2"/>
+            <button id="create_button" className="btn btn-primary" >Create</button>
+          </div>
+        </form>
       </div>
     );
   }

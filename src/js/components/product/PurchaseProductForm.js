f2bfedcc68aeedce1caa6c179375234577a38558
nik <nik_rb@yahoo.com>
2016-08-17 09:13:27 +0100

    fill out purchase product code

--- src/js/components/product/PurchaseProductForm.js
+++ src/js/components/product/PurchaseProductForm.js
@@ -1,40 +1,58 @@
 import React from 'react';
 
 export default class PurchaseProductForm extends React.Component{
+  constructor(){
+    super();
+  }
+  handleAmount( e){
+    this.props.changeAmount( e.target.value);
+    /*
+    console.log( "handleAmount:", e.target.value);
+    const val = parseFloat( e.target.value);
+    if( Number.isNaN( val)){
+      console.log( "amount is not a valid number");
+    } else {
+      console.log( "new amount is :", val);
+      this.setState( { amount: val});
+    }
+    console.log( "form state:", this.state);
+    */
+  }
   render() {
     return (
         <form className="well">
           <div className="form-group" >
             <label htmlFor="category" className="col-sm-2 control-label">Category</label>
-            <select id="category" className="input-sm" >
+            <select id="category" className="input-sm"  onChange={this.props.productChange}>
               {this.props.categories.map( cat =>
                 <option key={cat._id} value={cat.title}>{cat.title}</option>)}
             </select>
           </div>
           <div className="form-group" >
             <label htmlFor="product_name" className="col-sm-2 control-label">Name</label>
-            <input id="product-name" className="input-sm" />
+            <input id="product_name" className="input-sm" onChange={this.handleAmount.bind(this)}/>
           </div>
           <div className="form-group" >
             <label htmlFor="amount" className="col-sm-2 control-label">Amount</label>
-            <input id="amount" className="input-sm" />
+            <input id="amount" className="input-sm" onChange={this.handleAmount.bind(this)} />
           </div>
           <div className="form-group" >
             <label htmlFor="cost_per" className="col-sm-2 control-label" >Cost Per</label>
-            <input id="cost_per" className="input-sm"/>
-            <select className="input-sm">
-              <option>item</option>
-              <option>kilo</option>
-              <option>gram</option>
+            <input id="cost_per" className="input-sm" onChange={this.props.productChange}/>
+            <select id="item_or_weight" className="input-sm">
+              <option value="item">item</option>
+              <option value="kilo">kilo</option>
+              <option value="gram">gram</option>
             </select>
           </div>
           <div className="form-group" >
             <label htmlFor="cost" className="col-sm-2 control-label" >Cost</label>
-            <input className="input-sm" />
+            <input id="cost" className="input-sm"  onChange={this.props.productChange}/>
           </div>
           <div className="form-group">
             <label htmlFor="create_button" className="col-sm-2"/>
-            <button id="create_button" className="btn btn-primary" >Create</button>
+            <button id="create_button" className="btn btn-primary"
+                    onClick={this.props.createProduct}>Create</button>
           </div>
         </form>
     );
--- src/js/pages/Purchase.js
+++ src/js/pages/Purchase.js
@@ -3,30 +3,93 @@ import React from "react";
 import PurchaseProductForm from "../components/product/PurchaseProductForm";
 import ProductStore from "../stores/ProductStore";
 import * as ProductActions from "../actions/ProductActions";
+import CategoryStore from "../stores/CategoryStore";
+import * as CategoryActions from "../actions/CategoryActions";
 
 export default class Purchase extends React.Component {
   constructor(){
     super();
     this.getCategories = this.getCategories.bind(this);
     this.state = {
-      categories : []
+      categories : [],
+      category : "",
+      name : "",
+      amount: 12, // number of items or weight
+      cost_total : 0,
+      weight_total: 0,
+      unit_total: 0,
+      item_or_weight:"item",
+      cost_per: 0 // price per iten/weight
     };
   }
+  setTotals( ){
+    const {cost_per} = this.state;
+    if( this.state.item_or_weight === "item"){
+      this.setState( { unit_total: this.state.amount, weight_total: 0});
+      this.setState( { cost_total: cost_per * this.state.unit_total});
+    } else {
+      this.setState( { unit_total: 0, weight_total: this.state.amount});
+      this.setState( { cost_total: cost_per * this.state.weight_total});
+    }
+  }
+  changeAmount( amt){
+    console.log( "change amount");
+    const val = parseFloat( amt);
+    if( !Number.isNaN( val)){
+      console.log( "set amount:", val);
+      this.setState( {amount: val });
+      //  this.setTotals();
+    }
+    console.log( "current state:", this.state);
+  }
+  productChange(e){
+    console.log( "product change state:", this.state);
+    switch( e.target.id){
+      case "product-name": {
+        this.setState( {name: e.target.value});
+      }
+      case "item_or_weight": {
+        this.setState( { item_or_weight: e.target.value});
+        this.setTotals();
+      }
+      case "cost_per": {
+        const val = parseFloat( e.target.value);
+        if( !Number.isNaN( val)){
+          this.setState( {cost_per: val });
+          this.setTotals();
+        }
+      }
+    }
+  }
   componentWillMount(){
-    ProductStore.on( "change", this.getCategories);
-    ProductActions.loadProductCategories();
+    ProductStore.on( "change", this.productCreated);
+    CategoryStore.on( "change", this.getCategories);
+    CategoryActions.loadProductCategories();
   }
   componentWillUnmount(){
-    ProductStore.removeListener( 'change', this.getCategories);
+    CategoryStore.removeListener( 'change', this.getCategories);
+    ProductStore.removeListener( "change", this.productCreated);
   }
   getCategories(){
-    this.setState( { categories: ProductStore.getCategories()});
+    this.setState( { categories: CategoryStore.getAll()});
+  }
+  productCreated(){
+    console.log( "create new product");
+    // TODO: put up a message bar with close button
+  }
+  createProduct( e){
+    const { category, name, cost_total, weight_total, unit_total} = this.state;
+    console.log( "creating product name:", this.state);
+    // ProductActions.createProduct( category, name, cost_total, weight_total, unit_total);
   }
   render(){
     return (
       <div className="container" >
         <h1>Purchase</h1>
-        <PurchaseProductForm categories={this.state.categories} />
+        <PurchaseProductForm categories={this.state.categories}
+                            createProduct={this.createProduct.bind(this)}
+                            productChange={this.productChange.bind(this)}
+                            changeAmount={this.changeAmount.bind(this)}/>
       </div>
     );
   }

5a673b3b799bc6b0f38f56737684983926a007ba
nik <nik_rb@yahoo.com>
2016-08-17 23:46:24 +0100

    update mongo population to reflect changes wrt create product

--- scripts/init.mongo.js
+++ scripts/init.mongo.js
@@ -5,12 +5,8 @@ var db = new Mongo().getDB("productsdb");
 db.product.remove({});
 
 db.product.insert([
-  {category: 'Sporting Goods', price: '49.99', stocked: true, name: 'Football'},
-  {category: 'Sporting Goods', price: '9.99', stocked: true, name: 'Baseball'},
-  {category: 'Sporting Goods', price: '29.99', stocked: false, name: 'Basketball'},
-  {category: 'Electronics', price: '99.99', stocked: true, name: 'iPod Touch'},
-  {category: 'Electronics', price: '399.99', stocked: false, name: 'iPhone 5'},
-  {category: 'Electronics', price: '199.99', stocked: true, name: 'Nexus 7'}
+  { "category" : "Food", "name" : "carrot", "cost_total" : "0.60",
+    "weight_total" : 6, "unit_total" : 0 }
 ]);
 db.category.remove({});
 db.category.insert( [
